db42af370dd3ef82e3cc3c519f3398b5f083b7b3
joao.bitencourt <joao.bitencourt@useall.com.br>
2022-05-31 11:56:34 -0300

    adicionado dashboard

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable () {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>{new Date().toLocaleString()}</td>
+                    </tr>
+                    <tr>
+                        <td>aluguel</td>
+                        <td className="withdraw">- R$12.000,00</td>
+                        <td>casa</td>
+                        <td>{new Date().toLocaleString()}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
