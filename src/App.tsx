8b5c41485d4304477d4991b4a8cf7caa2faf8b41
joao.bitencourt <joao.bitencourt@useall.com.br>
2022-06-03 16:23:25 -0300

    adicionado modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
-  return (
-    <>
-      <Header/>
-      <Dashboard/>
-      <GlobalStyle />
-    </>
-  );
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
+    return (
+        <>
+            <Header 
+                onOpenNewTransactionModal={handleOpenNewTransactionModal}
+            />
+
+            <Dashboard />
+
+            <NewTransactionModal 
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}
+            />
+
+            <GlobalStyle />
+        </>
+    );
 }
